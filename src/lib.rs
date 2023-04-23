0045a441ca294beed3fc3d5731a44f9185c8cee8
Tony Rizko <tony@rizkocircle.com>
2023-04-22 17:18:40 -0700

    Add Shop struct with inventory

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,55 @@
 #![allow(dead_code)]
 
-#[derive(Debug, PartialEq)]
-struct Card {
+#[derive(Debug, PartialEq, Clone)]
+pub struct Card {
     name: String,
-    desc: String,
+    attack: u32,
+    health: u32,
 }
 
 impl Card {
-    fn new(name: &str, desc: &str) -> Self {
+    pub fn new(name: &str, attack: u32, health: u32) -> Self {
         Card {
             name: name.to_string(),
-            desc: desc.to_string(),
+            attack,
+            health,
         }
     }
 }
 
 #[derive(Debug, PartialEq)]
-struct Player {
+pub struct Player {
     board: Vec<Card>,
     hand: Vec<Card>,
 }
 
 impl Player {
-    fn new(hand: Vec<Card>, board: Vec<Card>) -> Self {
+    pub fn new(hand: Vec<Card>, board: Vec<Card>) -> Self {
         Player { board, hand }
+    }
+}
+
+#[derive(Debug)]
+pub struct Shop {
+    inventory: Vec<Card>,
+}
+
+impl Shop {
+    pub fn new() -> Self {
+        Shop { inventory: vec![] }
+    }
+
+    pub fn from_card_options(card_options: &[Card]) -> Self {
+        let mut inventory: Vec<Card> = vec![];
+
+        // build inventory for shop
+        for card in card_options {
+            for _ in 0..10 {
+                inventory.push(card.clone());
+            }
+        }
+
+        Shop { inventory }
     }
 }
 
@@ -34,8 +60,8 @@ mod test {
 
     #[test]
     fn card_works() {
-        let card_1 = Card::new("cat", "a cat");
-        let card_2 = Card::new("cat", "a cat");
+        let card_1 = Card::new("cat", 1, 2);
+        let card_2 = Card::new("cat", 1, 2);
         assert_eq!(card_1, card_2);
     }
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use autorank::Shop;
+use autorank::Card;
+
 fn main() {
-    println!("Hello, world!");
+    let card_options: &[Card] = &[
+        Card::new("cat", 1, 2), 
+        Card::new("dog", 2, 1),
+    ];
+
+    let shop = Shop::from_card_options(card_options);
+
+    println!("{:?}", shop);
 }
