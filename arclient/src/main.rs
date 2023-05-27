4f90a7d035bcc6b7c4bd8e44df56c0c7ab608adf
Tony Rizko <tony@rizkocircle.com>
2023-05-26 19:42:12 -0700

    Add initial frontend code

--- arclient/src/main.rs
+++ arclient/src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+            <h1>{ "Autorank" }</h1>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
